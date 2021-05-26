53d316940bb71587716be3513519bbc8f18ac5da
ruanvalente <ruanopensuselinux@gmail.com>
2021-05-26 14:33:04 -0300

    feat: added example state in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./styles/global.scss";
 
 import { RespositoryList } from "./components/RepositoryList";
+import { Counter } from "./components/Conuter";
 
 export function App() {
   return (
     <>
       <RespositoryList />
+      <Counter />
     </>
   );
 }
