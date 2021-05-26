8f9c20c01207c0a15a219c374a47ea2979257e4d
ruanvalente <ruanopensuselinux@gmail.com>
2021-05-26 13:57:40 -0300

    feat: example of property

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,44 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "React",
+  description: "Description",
+  link: "",
+};
+
 export function RespositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>Repository name</strong>
-          <p>Repository description</p>
-
-          <a href="" target="_blank" rel="noopener noreferrer">
-            Repository acess
-          </a>
-        </li>
-
-        <li>
-          <strong>Repository name</strong>
-          <p>Repository description</p>
-
-          <a href="" target="_blank" rel="noopener noreferrer">
-            Repository acess
-          </a>
-        </li>
-
-        <li>
-          <strong>Repository name</strong>
-          <p>Repository description</p>
-
-          <a href="" target="_blank" rel="noopener noreferrer">
-            Repository acess
-          </a>
-        </li>
-
-        <li>
-          <strong>Repository name</strong>
-          <p>Repository description</p>
-
-          <a href="" target="_blank" rel="noopener noreferrer">
-            Repository acess
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem />
       </ul>
     </section>
   );
